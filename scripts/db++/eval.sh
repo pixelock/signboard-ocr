964bab965435a2b299d7921f9b932af5b171a3a6
pixelock <95493953+pixelock@users.noreply.github.com>
2022-11-12 10:09:20 +0800

    feat(training): update

--- scripts/db++/eval.sh
+++ scripts/db++/eval.sh
@@ -1,1 +1,5 @@
 python PaddleOCR/tools/eval.py -c configs/det/db++/normal-d1.yml  -o Global.checkpoints="output/det/db++/normal/d1/best_accuracy"
+
+python PaddleOCR/tools/eval.py -c configs/det/db++/normal-d2.yml  -o Global.checkpoints="output/det/db++/normal/d2/best_accuracy"
+
+python PaddleOCR/tools/eval.py -c configs/det/db++/normal-d3.yml  -o Global.checkpoints="output/det/db++/normal/d3/best_accuracy"
--- scripts/db++/train.sh
+++ scripts/db++/train.sh
@@ -1,3 +1,6 @@
 python PaddleOCR/tools/train.py -c configs/det/db++/normal-d1.yml  -o Global.pretrained_model="plm/db++/ResNet50_dcn_asf_synthtext_pretrained"
+
 python PaddleOCR/tools/train.py -c configs/det/db++/normal-d2.yml  -o Global.pretrained_model="plm/db++/ResNet50_dcn_asf_synthtext_pretrained"
+
 python PaddleOCR/tools/train.py -c configs/det/db++/normal-d3.yml  -o Global.pretrained_model="plm/db++/ResNet50_dcn_asf_synthtext_pretrained"
+python PaddleOCR/tools/train.py -c configs/det/db++/normal-d3.yml  -o Global.checkpoints="output/det/db++/normal/d3/latest"
