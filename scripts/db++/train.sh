93ed9ca35d4c567c2ccb07afbf72bb316cd30d08
pixelock <95493953+pixelock@users.noreply.github.com>
2022-11-15 00:04:06 +0800

    feat(training): update

--- scripts/db++/train.sh
+++ scripts/db++/train.sh
@@ -1,6 +1,11 @@
-python PaddleOCR/tools/train.py -c configs/det/db++/normal-d1.yml  -o Global.pretrained_model="plm/db++/ResNet50_dcn_asf_synthtext_pretrained"
+python PaddleOCR/tools/train.py -c configs/det/db++/normal-d1.yml -o Global.pretrained_model="plm/db++/ResNet50_dcn_asf_synthtext_pretrained"
 
-python PaddleOCR/tools/train.py -c configs/det/db++/normal-d2.yml  -o Global.pretrained_model="plm/db++/ResNet50_dcn_asf_synthtext_pretrained"
+python PaddleOCR/tools/train.py -c configs/det/db++/normal-d2.yml -o Global.pretrained_model="plm/db++/ResNet50_dcn_asf_synthtext_pretrained"
 
-python PaddleOCR/tools/train.py -c configs/det/db++/normal-d3.yml  -o Global.pretrained_model="plm/db++/ResNet50_dcn_asf_synthtext_pretrained"
-python PaddleOCR/tools/train.py -c configs/det/db++/normal-d3.yml  -o Global.checkpoints="output/det/db++/normal/d3/latest"
+python PaddleOCR/tools/train.py -c configs/det/db++/normal-d3.yml -o Global.pretrained_model="plm/db++/ResNet50_dcn_asf_synthtext_pretrained"
+python PaddleOCR/tools/train.py -c configs/det/db++/normal-d3.yml -o Global.checkpoints="output/det/db++/normal/d3/latest"
+
+python PaddleOCR/tools/train.py -c configs/det/db++/eastcrop640-d3.yml -o Global.pretrained_model="plm/db++/ResNet50_dcn_asf_synthtext_pretrained"
+python PaddleOCR/tools/train.py -c configs/det/db++/eastcrop640-d3.yml -o Global.checkpoints="output/det/db++/eastcrop640/d3/latest"
+python PaddleOCR/tools/train.py -c configs/det/db++/eastcrop640-adam-d3.yml -o Global.pretrained_model="plm/db++/ResNet50_dcn_asf_synthtext_pretrained"
+python PaddleOCR/tools/train.py -c configs/det/db++/eastcrop640-adam-d3.yml -o Global.checkpoints="output/det/db++/eastcrop640/adam-d3/latest"
--- scripts/ppocr/student/eval.sh
+++ scripts/ppocr/student/eval.sh
@@ -1,3 +1,6 @@
 python PaddleOCR/tools/eval.py -c configs/det/ppocr/student-d1.yml  -o Global.checkpoints="plm/ppocr/ch_PP-OCRv3_det_distill_train/student"
+
 python PaddleOCR/tools/eval.py -c configs/det/ppocr/student-d1.yml  -o Global.checkpoints="output/det/ppocr/student/d1/best_accuracy"
+python PaddleOCR/tools/eval.py -c configs/det/ppocr/student-d1.yml  -o Global.checkpoints="output/det/ppocr/student/d1/best_accuracy" PostProcess.thresh=0.3 PostProcess.box_thresh=0.4 PostProcess.unclip_ratio=2.0
+
 python PaddleOCR/tools/eval.py -c configs/det/ppocr/student-d1_1.yml  -o Global.checkpoints="output/det/ppocr/student/d1/best_accuracy"
