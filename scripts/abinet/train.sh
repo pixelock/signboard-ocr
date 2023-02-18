642cfd4699c809efea46dbb793a13b520994a1ee
pixelock <95493953+pixelock@users.noreply.github.com>
2023-02-18 11:03:02 +0800

    feat(abinet): training

--- scripts/abinet/train.sh
+++ scripts/abinet/train.sh
@@ -1,2 +1,5 @@
+# 不使用预训练模型训练
+python PaddleOCR/tools/train.py -c configs/rec/abinet/r45-scratch-d1.yml
+# 使用预训练模型(英文数据集上训练)训练
 python PaddleOCR/tools/train.py -c configs/rec/abinet/r45-d1.yml -o Global.pretrained_model="plm/rec/rec_r45_abinet_train/abinet_vl_pretrained"
-python PaddleOCR/tools/train.py -c configs/rec/abinet/r45-d1.yml -o Global.checkpoints="output/det/db++/eastcrop640/d2/latest"
+python PaddleOCR/tools/train.py -c configs/rec/abinet/r45-d1.yml -o Global.checkpoints="output/rec/abinet/r45/d1/latest"
