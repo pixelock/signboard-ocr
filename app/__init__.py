7803c4b04e340c3787a563ad53c2e7bdfdc3a020
pixelock <95493953+pixelock@users.noreply.github.com>
2023-02-23 09:12:36 +0800

    feat(app): add service

---
+++ app/__init__.py
@@ -0,0 +1,9 @@
+# -*- coding: utf-8 -*-
+
+"""
+@Author  : pixelock
+@File    : __init__.py
+@Date    : 2023/2/21 23:07 
+"""
+
+from flask import Flask
--- scripts/abinet/train.sh
+++ scripts/abinet/train.sh
@@ -7,3 +7,7 @@ python PaddleOCR/tools/train.py -c configs/rec/abinet/r45-d1.yml -o Global.check
 python PaddleOCR/tools/train.py -c configs/rec/abinet/r45-d6.yml -o Global.pretrained_model="plm/rec/rec_r45_abinet_train/abinet_vl_pretrained"
 python PaddleOCR/tools/train.py -c configs/rec/abinet/r45-d6.yml -o Global.checkpoints="output/rec/abinet/r45/d6/latest"
 python PaddleOCR/tools/train.py -c configs/rec/abinet/r45-d7.yml -o Global.pretrained_model="plm/rec/rec_r45_abinet_train/abinet_vl_pretrained"
+python PaddleOCR/tools/train.py -c configs/rec/abinet/r45-d7.yml -o Global.checkpoints="output/rec/abinet/r45/d7/latest"
+# 使用Scene Text Recognition Data Augmentation, 做数据增强训练
+python PaddleOCR/tools/train.py -c configs/rec/abinet/r45-straug-d1.yml -o Global.pretrained_model="plm/rec/rec_r45_abinet_train/abinet_vl_pretrained"
+python PaddleOCR/tools/train.py -c configs/rec/abinet/r45-straug-d1.yml -o Global.checkpoints="output/rec/abinet/r45-straug/d1/latest"
